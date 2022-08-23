0ab1b4eeb96a7ca59055f463db345db10ddde775
obaby <obaby.lh@gmail.com>
2022-08-23 21:43:47 +0800

    更新图标

--- wp-useragent/wp-useragent.php
+++ wp-useragent/wp-useragent.php
@@ -251,7 +251,7 @@ function wpua_display_useragent($wpua_wrapper_div = false) {
     }
      if ($wpua_show_ip_address === 'true') {
      	 if ($wpua_show_text_icons === 'icons_and_text' )
-		$wpua_useragent.= '  '.  '<img src="'.WP_PLUGIN_URL.'/wp-useragent/img/' .$wpua_icon_size . '/network/ip2.png" title="ip address" alt="ip address" class="ip-address" width="$wpua_icon_size" height="$wpua_icon_size" />';
+		$wpua_useragent.= '  '.  '<img src="'.WP_PLUGIN_URL.'/wp-useragent/img/' .$wpua_icon_size . '/network/ip3.png" title="ip address" alt="ip address" class="ip-address" width="$wpua_icon_size" height="$wpua_icon_size" />';
 		 $wpua_useragent.= '  '. ip2unkonw($ip);
 	if ($wpua_show_text_icons === 'text')
 		$wpua_useragent.= '  '. ip2unkonw($ip);
